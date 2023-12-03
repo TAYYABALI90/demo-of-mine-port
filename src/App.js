936684a296c287c5316f3e5e49c72282a061fd60
tayyab_20 <tayyabali78013@gmail.com>
2023-12-03 13:10:09 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+//Component
+import Main from "./Components/main/Main";
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <Main />
+
+    </div>
+
+  );
+
+}
+
+export default App;
---
+++ src/Components/main/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//Components
+import Navbar from '../navbar/Navbar';
+import Home from '../home/Home';
+
+const Main = () => {
+
+    return (
+
+        <div className='Main'>
+
+            <Navbar />
+            
+            <Home />
+
+        </div>
+
+    )
+
+}
+
+export default Main;
